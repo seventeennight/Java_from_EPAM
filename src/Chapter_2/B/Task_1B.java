cfda16d781306d0b91dcefe2b72c32c150ccabe0
lex <7teen.night@gmail.com>
2024-12-14 13:18:48 +0800

    update

--- src/Chapter_2/B/Task_1B.java
+++ src/Chapter_2/B/Task_1B.java
@@ -1,4 +1,12 @@
 package Chapter_2.B;
 
 public class Task_1B {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d x %d = %2d\t", i, j, i * j);
+            }
+            System.out.println();
+        }
+    }
 }
